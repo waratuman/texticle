aeeebf849f43ba48916f73b623198095d0df1904
James R. Bracy <waratuman@gmail.com>
2013-09-08 00:28:11 -0700

    Return everything for a nil search.

--- test/texticle_test.rb
+++ test/texticle_test.rb
@@ -21,35 +21,35 @@ class TexticleTest < ActiveSupport::TestCase
     assert_equal [Author.arel_table['id'], [Book.arel_table['id'], Book.arel_table['title']]], Texticle.arel_columns(Author, [:id, {:books => [:id, :title]}])
   end
   
-  test 'Texticle::searchable_columns returns string and text columns' do
+  test 'Texticle#searchable_columns returns string and text columns' do
     assert_equal ['title', 'author', 'slug'], Book.searchable_columns
   end
   
-  test 'Texticle::searchable_columns returns overridden columns' do
+  test 'Texticle#searchable_columns returns overridden columns' do
     assert_equal [[:title, :author]], Novel.searchable_columns
   end
   
-  test 'Texticle::ts_columns returns string and text columns' do
+  test 'Texticle#ts_columns returns string and text columns' do
     assert_equal [[Book.arel_table['title']], [Book.arel_table['author']], [Book.arel_table['slug']]], Book.ts_columns
   end
   
-  test 'Texticle::ts_columns returns overridden columns' do
+  test 'Texticle#ts_columns returns overridden columns' do
     assert_equal [[Novel.arel_table['title'], Novel.arel_table['author']]], Novel.ts_columns
   end
   
-  test 'Texticle::ts_language returns english by default' do
+  test 'Texticle#ts_language returns english by default' do
     assert_equal 'english', Book.ts_language
   end
   
-  test 'Texticle::ts_vectors returns ts_vectors' do
+  test 'Texticle#ts_vectors returns ts_vectors' do
     assert_equal ["to_tsvector('english', \"books\".\"title\" :: text)", "to_tsvector('english', \"books\".\"author\" :: text)", "to_tsvector('english', \"books\".\"slug\" :: text)"], Book.ts_vectors.map(&:to_sql)
   end
 
-  test 'Texticle::ts_vectors with joining text fields' do
+  test 'Texticle#ts_vectors with joining text fields' do
     assert_equal ["to_tsvector('english', \"books\".\"title\" :: text || \"books\".\"author\" :: text)"], Novel.ts_vectors.map(&:to_sql)
   end
   
-  test 'Texticle::ts_query returns query' do
+  test 'Texticle#ts_query returns query' do
     assert_equal "to_tsquery('english', 'dorian:* & gray:*' :: text)", Book.ts_query('dorian gray').to_sql
   end
 
@@ -57,23 +57,23 @@ class TexticleTest < ActiveSupport::TestCase
     assert_equal "to_tsquery('english', 'dorian:* & gray:*' :: text)", Book.ts_query('dorian & gray ():|!&*\'').to_sql
   end
   
-  test 'Texticle::ts_query with array returns query' do
+  test 'Texticle#ts_query with array returns query' do
     assert_equal "to_tsquery('english', 'dorian:* & gray:*' :: text)", Book.ts_query(['dorian', 'gray']).to_sql
   end
   
-  test 'Texticle::ts_query with integer returns query' do
+  test 'Texticle#ts_query with integer returns query' do
     assert_equal "to_tsquery('english', '0:*' :: text)", Book.ts_query(0).to_sql
   end
   
-  test 'Texticle::ts_order returns ordering' do
+  test 'Texticle#ts_order returns ordering' do
     assert_equal "LEAST(\"books\".\"title\" :: text <-> 'dorian gray' :: text, \"books\".\"author\" :: text <-> 'dorian gray' :: text, \"books\".\"slug\" :: text <-> 'dorian gray' :: text)", Book.ts_order('dorian gray').to_sql
   end
   
-  test 'Texticle::ts_order returns ordering with custom searchable_columns' do
+  test 'Texticle#ts_order returns ordering with custom searchable_columns' do
     assert_equal "\"books\".\"title\" :: text || \"books\".\"author\" :: text <-> 'dorian gray' :: text", Novel.ts_order('dorian gray').to_sql
   end
   
-  test 'Texticle::search' do
+  test 'Texticle#search' do
     assert_equal (<<-SQL).strip.gsub(/\s+/, ' '), Book.search('dorian gray').to_sql.gsub(/\s+/, ' ')
       SELECT "books".*
       FROM "books"
@@ -86,7 +86,7 @@ class TexticleTest < ActiveSupport::TestCase
     SQL
   end
   
-  test 'Texticle::search with custom searhable_columns' do
+  test 'Texticle#search with custom searhable_columns' do
     assert_equal (<<-SQL).strip.gsub(/\s+/, ' '), Novel.search('dorian gray').to_sql.gsub(/\s+/, ' ')
       SELECT "books".*
       FROM "books"
@@ -95,7 +95,7 @@ class TexticleTest < ActiveSupport::TestCase
     SQL
   end
   
-  test 'Texticle::search with integer columns' do
+  test 'Texticle#search with integer columns' do
     assert_equal (<<-SQL).strip.gsub(/\s+/, ' '), Biography.search(0).to_sql.gsub(/\s+/, ' ')
       SELECT "books".*
       FROM "books"
@@ -104,7 +104,7 @@ class TexticleTest < ActiveSupport::TestCase
     SQL
   end
   
-  test 'Texticle::search with array' do
+  test 'Texticle#search with array' do
     assert_equal (<<-SQL).strip.gsub(/\s+/, ' '), Book.search(['dorian', 'gray']).to_sql.gsub(/\s+/, ' ')
       SELECT "books".*
       FROM "books"
@@ -117,7 +117,7 @@ class TexticleTest < ActiveSupport::TestCase
     SQL
   end
   
-  test 'Texticle::search with join' do
+  test 'Texticle#search with join' do
     assert_equal (<<-SQL).strip.gsub(/\s+/, ' '), Author.search('dorian gray').to_sql.gsub(/\s+/, ' ')
       SELECT "authors".*
       FROM "authors"
@@ -135,24 +135,9 @@ class TexticleTest < ActiveSupport::TestCase
       ORDER BY LEAST("books"."title" :: text <-> 'dorian gray' :: text, "authors"."id" :: text || "authors"."name" :: text <-> 'dorian gray' :: text)
     SQL
   end
-  
-  test 'Texticle::ts_arel_columns returns arel columns when given symbol' do
-    assert_equal Book.arel_table['id'], Texticle.arel_columns(Book, 'id')
-  end
-  
-  test 'Texticle::ts_arel_columns returns arel columns when given array' do
-    assert_equal [Book.arel_table['id'], Book.arel_table['title']], Texticle.arel_columns(Book, ['id', 'title'])
-  end
-  
-  test 'Texticle::ts_arel_columns returns arel columns when given hash' do
-    assert_equal [Book.arel_table['title']], Texticle.arel_columns(Author, {'books' => 'title'})
-    assert_equal [Book.arel_table['title'], Book.arel_table['id']], Texticle.arel_columns(Author, {'books' => ['title', 'id']})
-  end
-  
-  test 'Texticle::ts_arel_columns returns arel columns when given array with symbols and hash' do
-    assert_equal [Author.arel_table['id'], [Book.arel_table['title']]], Texticle.arel_columns(Author, ['id', {'books' => 'title'}])
-    assert_equal [Author.arel_table['id'], [Book.arel_table['id']], [Book.arel_table['title']]], Texticle.arel_columns(Author, ['id', {'books' => 'id'}, {'books' => 'title'}])
-    assert_equal [Author.arel_table['id'], [Book.arel_table['id'], Book.arel_table['title']]], Texticle.arel_columns(Author, ['id', {'books' => ['id', 'title']}])
+
+  test 'Texticle#search with nil' do
+    assert_equal 'SELECT "books".* FROM "books"', Book.search(nil).to_sql
   end
 
 end
